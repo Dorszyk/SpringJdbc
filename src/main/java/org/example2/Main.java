65bfa891e526bba59c801052b672232992724245
Piotr Dorszynski <piotr.dorszynski@gmail.com>
2023-02-11 15:56:50 +0100

    simpleJdbcCallExample

---
+++ src/main/java/org/example2/Main.java
@@ -0,0 +1,15 @@
+package org.example2;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(DataSourceConfiguration.class);
+
+        SimpleJdbcCallExample example = context.getBean(SimpleJdbcCallExample.class);
+        example.example();
+
+    }
+}
